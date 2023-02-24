2c4a115243eee10dbe329a52c883e92c4f0e9a2f
lawsoni5969 <124749542+lawsoni5969@users.noreply.github.com>
2023-02-23 19:39:41 -0500

    Module 4: Python 2 Lab 1 2/23/2023

---
+++ P2LAB1_LawsonIsaac.py
@@ -0,0 +1,6 @@
+mpg = float(input(''))
+gasCost = float(input(''))
+twentyMiles = (20 / mpg) * gasCost
+seventyFive = (75 / mpg) * gasCost
+fiveHundred = (500 / mpg) * gasCost
+print(f'{twentyMiles:.2f} {seventyFive:.2f} {fiveHundred:.2f}')
