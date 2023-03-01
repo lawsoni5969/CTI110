c6e5487e0c44332f80c65d503e5cd7833fc9089b
lawsoni5969 <124749542+lawsoni5969@users.noreply.github.com>
2023-02-28 19:28:32 -0500

    Module 4: Python 2 Lab 2 2/28/2023

---
+++ P2LAB2_LawsonIsaac.py
@@ -0,0 +1,10 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+''' Type your code here. '''
+prod1 = num1 * num2 * num3 * num4
+avg1 = (num1 + num2 + num3 + num4) / 4
+print(f'{prod1:.0f} {avg1:.0f}')
+print(f'{prod1:.3f} {avg1:.3f}')
