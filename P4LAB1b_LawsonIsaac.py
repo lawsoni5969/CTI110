360e1433e6a8f44456eb91270b997a13ade7afe8
lawsoni5969 <124749542+lawsoni5969@users.noreply.github.com>
2023-03-22 20:41:16 -0400

    Module 6: Python 4 Lab 1b 3/22/2023

---
+++ P4LAB1b_LawsonIsaac.py
@@ -0,0 +1,24 @@
+import turtle
+turtleScreen = turtle.Screen()
+t = turtle.Turtle()
+t.pensize(4)
+t.pencolor("red")
+
+t.forward(100)
+t.left(180)
+t.forward(50)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(50)
+t.left(180)
+t.forward(100)
+
+t.penup()
+
+t.forward(80)
+t.left(180)
+t.pendown()
+t.forward(70)
+t.right(90)
+t.forward(100)
